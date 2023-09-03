4cbca799fdaf25ef7b82faf6c3e96963b887d33b
linglu.zym <linglulu0112@gmail.com>
2023-09-03 10:45:44 +0800

    添加EnvData、配置文件yml

---
+++ sanxiaweb/src/main/java/com/sanxia/data/pojo/EnvData.java
@@ -0,0 +1,95 @@
+package com.sanxia.data.pojo;
+
+public class EnvData {
+    private String envId;
+
+    private String parentId;
+
+    private String envName;
+
+    private String envType;
+
+    private String envTypeName;
+
+    private String envCoverUrl;
+
+    private String collectTime;
+
+    private String envirParamType;
+
+    private String envirParamValue;
+
+    public String getEnvId() {
+        return envId;
+    }
+
+    public void setEnvId(String envId) {
+        this.envId = envId;
+    }
+
+    public String getParentId() {
+            return parentId;
+        }
+
+    public void setParentId(String parentId) {
+        this.parentId = parentId;
+    }
+
+    public String getEnvName() {
+        return envName;
+    }
+
+    public void setEnvName(String envName) {
+        this.envName = envName;
+    }
+
+    public String getEnvType() {
+        return envType;
+    }
+
+    public void setEnvType(String envType) {
+        this.envType = envType;
+    }
+
+    public String getEnvTypeName() {
+        return envTypeName;
+    }
+
+    public void setEnvTypeName(String envTypeName) {
+        this.envTypeName = envTypeName;
+    }
+
+    public String getEnvCoverUrl() {
+        return envCoverUrl;
+    }
+
+    public void setEnvCoverUrl(String envCoverUrl) {
+        this.envCoverUrl = envCoverUrl;
+    }
+
+    public String getCollectTime() {
+        return collectTime;
+    }
+
+    public void setCollectTime(String collectTime) {
+        this.collectTime = collectTime;
+    }
+
+    public String getEnvirParamType() {
+        return envirParamType;
+    }
+
+    public void setEnvirParamType(String envirParamType) {
+        this.envirParamType = envirParamType;
+    }
+
+    public String getEnvirParamValue() {
+        return envirParamValue;
+    }
+
+    public void setEnvirParamValue(String envirParamValue) {
+        this.envirParamValue = envirParamValue;
+    }
+}
+
+    
